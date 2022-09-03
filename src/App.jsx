b4320a63a6426ea54c28f9b2671b5c553dc80386
pedrou103 <pedroluca13.plm40@gmail.com>
2022-09-03 19:35:31 -0300

    Estrutura básica do projeto e definição das rotas do projeto

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Link, Outlet } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <nav id='navbar'>
+        <h2><Link to="/">Home</Link></h2>
+        <Link to="/movie/1">Movies</Link>
+        <Link to="/search">Search</Link>
+      </nav>
+      <h2>Movies Lib</h2>
+      <Outlet />
+    </div>
+  )
+}
+
+export default App
